4b0fa27ffb4519e4248b70658a72727e17bdbccd
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-04-19 15:17:55 -0500

    found all employees with a 'q' in their last name

--- where_exercises.sql
+++ where_exercises.sql
@@ -9,3 +9,6 @@ select * from employees where first_name in ('Irena', 'Vidya','Maya') ;
 # 3. Find all employees whose last name starts with 'E' — 7,330 rows.
 #Use like instead of =
 select * from employees where last_name like 'E%';
+
+#4. Find all employees with a 'q' in their last name — 1,873 rows.
+select * from employees where last_name like '%q%';
