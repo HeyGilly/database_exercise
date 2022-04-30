c94f1f14e9314332d47e34ee836ec2e956659612
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-04-30 16:36:14 -0500

    Worked on deleted only certain albums.

--- delete_exercise.sql
+++ delete_exercise.sql
@@ -1,13 +1,26 @@
-use codeup_test_db;
+USE codeup_test_db;
 
-#4.1
-delete from albums where release_date >= 1991;
-#4.2
-delete from albums where genre = 'disco';
-#4.3
-delete from albums where artist = 'whitney houston';
+# 4.a - output Albums released after 1991
+SELECT artist AS Albums_released_after_1991 FROM albums WHERE release_date > 1991;
 
-select * from albums;
+#4.b - output Albums with the genre 'disco'
+SELECT  artist as Artist, name as Album_Name FROM albums WHERE genre = 'disco';
 
-select *
-from albums where artist = 'whitney houston';
+#4.c - output Albums by Whitney Houston or any artist.
+SELECT artist as Artist, name as Album_Name from albums where artist = 'nirvana';
+
+#5 - Make sure to put appropriate captions before each SELECT.
+# { Check }
+
+# Transfer select into delete
+
+# SELECT artist AS Albums_released_after_1991 FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE release_date > 1991;
+# SELECT  artist as Artist, name as Album_Name FROM albums WHERE genre = 'disco';
+DELETE FROM albums WHERE genre = 'disco';
+# SELECT artist as Artist, name as Album_Name from albums where artist = 'nirvana';
+DELETE FROM albums WHERE artist ='nirvana';
+
+
+
+## Now run the code up top and you shall see that the data is deleted.
