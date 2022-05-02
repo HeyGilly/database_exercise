23dc572d8122307d7a14e998fb5a4828ecef26a2
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-05-01 20:35:28 -0500

    returned employees number, first and last name, and date of birth.

---
+++ aliases_exercises.sql
@@ -0,0 +1,19 @@
+#2. - Use the employees database.
+use employees;
+
+#3. - Return 10 employees in a result set named 'full_name'
+#     in the format of 'last name, first name' for each employee.
+SELECT concat(last_name, ' ', first_name) as full_name
+FROM employees
+limit 10;
+
+#4. - Add the date of birth for each employee as 'DOB' to the query.
+SELECT concat(last_name, ' ', first_name) full_name, birth_date DOB
+FROM employees
+limit 10;
+
+#5. - Update the query to format full name to include the employee
+#     number so it is formatted as 'employee number - last name, first name'.
+SELECT concat(emp_no,' - ',last_name, ', ', first_name) full_name, birth_date DOB
+FROM employees
+limit 10;
